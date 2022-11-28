614096ca64d4f4b59f3e60b338f512a4979cd494
Ken <kenblank150@gmail.com>
2022-11-28 17:48:20 +0100

    switched port to 3001

--- config/configuration.js
+++ config/configuration.js
@@ -1,13 +1,13 @@
 import fetch from 'node-fetch';
 
 async function getData(path, isPost) {
-  const get_response = await fetch(`http://localhost:3000/${path}`);
+  const get_response = await fetch(`http://localhost:3001/${path}`);
   const data = await get_response.json();
   isPost ? postData(path, data) : patchData(path, data);
 }
 
 async function postData(path, data) {
-  await fetch(`http://localhost:3000/${path}`, {
+  await fetch(`http://localhost:3001/${path}`, {
     method: 'POST',
     headers: {
       Accept: 'application/json',
@@ -18,7 +18,7 @@ async function postData(path, data) {
 }
 
 async function patchData(path, data) {
-  await fetch(`http://localhost:3000/${path}`, {
+  await fetch(`http://localhost:3001/${path}`, {
     method: 'PATCH',
     headers: {
       Accept: 'application/json',
